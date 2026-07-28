94f9e1a6751e57b12f520991de43dde93f2b8dea
Auke van Slooten <auke@muze.nl>
2026-07-28 09:21:24 +0200

    updated build

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "programming-for-wizards-bf6ecf2e65"
+const CACHE_NAME = "programming-for-wizards-f5eccd8a22"
 const PRECACHE_URLS = [
   "./assets/book.css",
   "./assets/book.js",
